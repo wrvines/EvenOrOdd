e46b60da59f987078b9a5852b3ca9b9127debd31
poopysmack <54606838+poopysmack@users.noreply.github.com>
2022-08-17 18:06:46 -0600

    finished

--- evenorodd.js
+++ evenorodd.js
@@ -3,8 +3,10 @@
 //prompt number and store it
 let userNumber = prompt("Please enter a number to see if it is Even or Odd.");
 
+//do the math to find out the remainder
 let result = userNumber % 2;
 
+//compare results
 if (result === 0) {
     console.log("Your number of " + userNumber + ", is even.");
 } else {
